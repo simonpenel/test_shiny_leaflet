929fbd0610219568050e24cf6a64bbbf7dd8b35c
Simon Penel <simon.penel@univ-lyon1.fr>
2025-03-12 10:51:30 +0100

    La selection sur la date fonctionne

--- mon_app2/app.R
+++ mon_app2/app.R
@@ -2,29 +2,40 @@ library(shiny)
 library(leaflet)
 library(RColorBrewer)
 
+# Masting data
+masting <- read.csv("../MASTREEplus_2024-06-26_V2.csv")
+
+
+# UI
 ui <- bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+
+  tags$style(type = "text/css",
+    "html, body {width:100%;height:100%}
+    #controls { background-color: #ddd; opacity: 0.5;"
+  ),
+
   leafletOutput("map", width = "100%", height = "100%"),
-    absolutePanel(top = 10, right = 10,
-    sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-      value = range(quakes$mag), step = 0.1
+  
+  absolutePanel(top = 10, right = 10, class = "panel panel-default",
+    sliderInput("range", "Year", min(masting$Year), max(masting$Year),
+      value = range(masting$Year), step = 1, sep ="", width=600
     ),
     selectInput("colors", "Color Scheme",
       rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
     ),
     checkboxInput("legend", "Show legend", TRUE),
 
-     absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-        width = 330, height = "auto",
+    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+      width = 330, height = "auto",
 
-        plotOutput("histCentile", height = 250),
-        plotOutput("scatterCollegeIncome", height = 250)
-        )
-
+      plotOutput("histCentile", height = 250),
+      plotOutput("scatterCollegeIncome", height = 250)
+    )
   )
 )
 
+# SERVER
 server <- function(input, output, session) {
 
   colorpal <- reactive({
@@ -35,45 +46,23 @@ server <- function(input, output, session) {
     # Use leaflet() here, and only include aspects of the map that
     # won't need to change dynamically (at least, not unless the
     # entire map is being torn down and recreated).
-    leaflet(quakes) %>% addTiles() %>%
-      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    leaflet(masting) %>% addTiles() %>%
+    fitBounds(~min(Longitude), ~min(Latitude), ~max(Longitude), ~max(Latitude))
   })
 
-  # Incremental changes to the map (in this case, replacing the
-  # circles when a new color is chosen) should be performed in
+  # Incremental changes to the map  should be performed in
   # an observer. Each independent set of things that can change
   # should be managed in its own observer.
 
-  data <- reactive({
-    file <- "../MASTREEplus_2022-02-03_V1.csv"
-    read.csv(file)
-  })
-
-
-  data <- read.csv("../MASTREEplus_2022-02-03_V1.csv")
   filteredData <- reactive({
-    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+    masting[masting$Year >= input$range[1] & masting$Year <= input$range[2],]
   })
-
-
-    filteredData <- reactive({
-    data
-  })
-  
 
   observe({
-    pal <- colorpal()
-
+    pal<-colorpal()
     leafletProxy("map", data = filteredData()) %>%
-      clearShapes() %>%
-      #addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
-      #  fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
-      #)
-          ## addCircles(radius = ~log(Value), weight = 1, color = "#777777",
-        ##fillColor = "black", fillOpacity = 0.7, popup = ~paste(Value)
-      ##) %>%
-                      addMarkers(label = ~as.character(Species),clusterOptions = markerClusterOptions()
-      ) 
+      clearMarkerClusters() %>%
+      addMarkers(label = ~paste(Species," ", Year),clusterOptions = markerClusterOptions())
   })
 }
 
